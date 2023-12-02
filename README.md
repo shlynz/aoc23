9397610cf02e7b141c153a059760c84d25b0a5cb
Shlynz <75536038+shlynz@users.noreply.github.com>
2023-12-02 12:37:01 +0100

    Tidied up day 01

--- README.md
+++ README.md
@@ -7,6 +7,15 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 <!--- advent_readme_stars table --->
 
 <!--- benchmarking table --->
+## Benchmarks
+
+| Day | Part 1 | Part 2 |
+| :---: | :---: | :---:  |
+| [Day 1](./src/bin/01.rs) | `201.5µs` | `1.4ms` |
+| [Day 2](./src/bin/02.rs) | `846.8µs` | `721.7µs` |
+
+**Total: 3.17ms**
+<!--- benchmarking table --->
 
 ---
 
--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,48 +1,51 @@
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let numbers: Result<u32, _> = input
-        .lines()
-        .into_iter()
-        .map(|val| {
-            let mut digits = val.matches(char::is_numeric).into_iter();
-            match (digits.next(), digits.last()) {
-                (Some(first), Some(last)) => first.to_owned() + last,
-                (Some(first), None) => first.to_owned() + first,
-                (_, _) => "0".to_owned(),
-            }
-        })
-        .map(|val| val.parse::<u32>())
-        .sum();
-    Some(numbers.ok()?)
+    Some(
+        input
+            .lines()
+            .into_iter()
+            .map(|val| {
+                let mut digits = val.matches(char::is_numeric).into_iter();
+                match (digits.next(), digits.last()) {
+                    (Some(first), Some(last)) => first.to_owned() + last,
+                    (Some(first), None) => first.to_owned() + first,
+                    (_, _) => "0".to_owned(),
+                }
+                .parse::<u32>()
+                .unwrap()
+            })
+            .sum(),
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let numbers: Result<u32, _> = input
-        .lines()
-        .into_iter()
-        .map(|line| {
-            line.replace("one", "one1one")
-                .replace("two", "two2two")
-                .replace("three", "three3three")
-                .replace("four", "four4four")
-                .replace("five", "five5five")
-                .replace("six", "six6six")
-                .replace("seven", "seven7seven")
-                .replace("eight", "eight8eight")
-                .replace("nine", "nine9nine")
-        })
-        .map(|val| {
-            let mut digits = val.matches(char::is_numeric).into_iter();
-            match (digits.next(), digits.last()) {
-                (Some(first), Some(last)) => first.to_owned() + last,
-                (Some(first), None) => first.to_owned() + first,
-                (_, _) => "0".to_owned(),
-            }
-        })
-        .map(|val| val.parse::<u32>())
-        .sum();
-    Some(numbers.ok()?)
+    Some(
+        input
+            .lines()
+            .into_iter()
+            .map(|line| {
+                let val = line
+                    .replace("one", "one1one")
+                    .replace("two", "two2two")
+                    .replace("three", "three3three")
+                    .replace("four", "four4four")
+                    .replace("five", "five5five")
+                    .replace("six", "six6six")
+                    .replace("seven", "seven7seven")
+                    .replace("eight", "eight8eight")
+                    .replace("nine", "nine9nine");
+                let mut digits = val.matches(char::is_numeric).into_iter();
+                match (digits.next(), digits.last()) {
+                    (Some(first), Some(last)) => first.to_owned() + last,
+                    (Some(first), None) => first.to_owned() + first,
+                    (_, _) => "0".to_owned(),
+                }
+                .parse::<u32>()
+                .unwrap()
+            })
+            .sum::<u32>(),
+    )
 }
 
 #[cfg(test)]
