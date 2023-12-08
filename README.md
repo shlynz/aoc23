56b814680b2fa7e37411e2351ddf0132d4b7a039
Shlynz <75536038+shlynz@users.noreply.github.com>
2023-12-08 16:41:41 +0100

    Crudely finished day 08

--- README.md
+++ README.md
@@ -11,15 +11,16 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
-| [Day 1](./src/bin/01.rs) | `203.2µs` | `1.4ms` |
-| [Day 2](./src/bin/02.rs) | `813.9µs` | `732.7µs` |
+| [Day 1](./src/bin/01.rs) | `202.9µs` | `1.4ms` |
+| [Day 2](./src/bin/02.rs) | `814.8µs` | `723.0µs` |
 | [Day 3](./src/bin/03.rs) | `1.4ms` | `1.1ms` |
-| [Day 4](./src/bin/04.rs) | `43.7µs` | `41.3µs` |
-| [Day 5](./src/bin/05.rs) | `25.8µs` | `-` |
-| [Day 6](./src/bin/06.rs) | `495.0ns` | `1.1µs` |
-| [Day 7](./src/bin/07.rs) | `582.5µs` | `623.1µs` |
+| [Day 4](./src/bin/04.rs) | `43.6µs` | `41.2µs` |
+| [Day 5](./src/bin/05.rs) | `25.7µs` | `-` |
+| [Day 6](./src/bin/06.rs) | `509.0ns` | `1.1µs` |
+| [Day 7](./src/bin/07.rs) | `577.6µs` | `619.1µs` |
+| [Day 8](./src/bin/08.rs) | `581.2µs` | `-` |
 
-**Total: 6.97ms**
+**Total: 7.53ms**
 <!--- benchmarking table --->
 
 ---
---
+++ src/bin/08.rs
@@ -0,0 +1,116 @@
+use itertools::{FoldWhile, Itertools};
+use std::collections::HashMap;
+
+advent_of_code::solution!(8);
+
+#[derive(Debug)]
+struct Node<'a> {
+    left: &'a [u8],
+    right: &'a [u8],
+}
+pub fn part_one(input: &str) -> Option<u32> {
+    let (directions, nodes) = input.split_once("\n\n").unwrap();
+    let nodes: HashMap<_, _> = nodes
+        .lines()
+        .map(str::as_bytes)
+        .map(|bytes| {
+            (
+                &bytes[0..=2],
+                Node {
+                    left: &bytes[7..=9],
+                    right: &bytes[12..=14],
+                },
+            )
+        })
+        .collect();
+
+    let starting_node = nodes.get("AAA".as_bytes()).unwrap();
+
+    Some(
+        directions
+            .chars()
+            .cycle()
+            .fold_while((0, starting_node), |(steps, last_node), next_dir| {
+                let steps = steps + 1;
+                let next_node_name = if next_dir == 'R' {
+                    last_node.right
+                } else {
+                    last_node.left
+                };
+                let next_node = nodes.get(next_node_name).unwrap();
+                if next_node_name == "ZZZ".as_bytes() {
+                    FoldWhile::Done((steps, next_node))
+                } else {
+                    FoldWhile::Continue((steps, next_node))
+                }
+            })
+            .into_inner()
+            .0 as u32,
+    )
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let (directions, node_lines) = input.split_once("\n\n").unwrap();
+    let node_lines = node_lines.lines().map(str::as_bytes).collect_vec();
+    let nodes: HashMap<_, _> = node_lines
+        .iter()
+        .map(|bytes| {
+            (
+                &bytes[0..=2],
+                Node {
+                    left: &bytes[7..=9],
+                    right: &bytes[12..=14],
+                },
+            )
+        })
+        .collect();
+
+    println!(
+        "Just use Wolframalpha with the following input:\nleast common multiple of {}",
+        node_lines
+            .iter()
+            .filter(|line| line[2] == b'A')
+            .map(|line| &line[0..=2])
+            .map(|node_name| nodes.get(node_name).unwrap())
+            .map(|node| {
+                directions
+                    .chars()
+                    .cycle()
+                    .fold_while((0, node), |(steps, last_node), next_dir| {
+                        let steps = steps + 1;
+                        let next_node_name = if next_dir == 'R' {
+                            last_node.right
+                        } else {
+                            last_node.left
+                        };
+                        let next_node = nodes.get(next_node_name).unwrap();
+                        if next_node_name[2] == b'Z' {
+                            FoldWhile::Done((steps, next_node))
+                        } else {
+                            FoldWhile::Continue((steps, next_node))
+                        }
+                    })
+                    .into_inner()
+                    .0
+            })
+            .join(" ")
+    );
+    Some(0)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(6));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(0));
+    }
+}
